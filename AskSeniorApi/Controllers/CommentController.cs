538009d4dfb162e49082c9bed546aed601fc7c50
simpleDevWeb3 <g64bit507@gmail.com>
2025-12-10 20:56:14 +0800

    fix bug get comment by user retrive wrong data and isVote always null

--- AskSeniorApi/Controllers/CommentController.cs
+++ AskSeniorApi/Controllers/CommentController.cs
@@ -27,7 +27,7 @@ public class CommentController : ControllerBase
     {
         userId = userId.Clean();
         postId = postId.Clean();
-        var commentsDto = await _commentService.GetCommentsAsync(current_user: userId, postId: postId);
+        var commentsDto = await _commentService.GetCommentsAsync(user_id: userId, postId: postId,current_user:userId);
         
         return Ok(commentsDto);
     }
